f00f2b08f2e03e41a76a3f22d1c05d53f66166c2
sdoncheva <95342130+sdoncheva@users.noreply.github.com>
2024-03-01 23:59:28 +0000

    updated chtan=bot and login oages

--- DjangoChatbot/chat/urls.py
+++ DjangoChatbot/chat/urls.py
@@ -1,9 +1,13 @@
 # chat/urls.py
-
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.chat_home, name='chat_home'),
     path('login/', views.login_view, name='login'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
